e1ed27c2f9fb51bdcec499fffdb08e52f87ba777
Sarah Stanton <sarah.g.stanton@gmail.com>
2017-04-05 19:37:46 -0700

    new map

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 "use strict";
 
-var mapboxAccessToken = '';
-var map = L.map('map').setView([21, 78], 4);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
-    id: 'mapbox.light',
-}).addTo(map);
+var mymap = L.map('map').setView([21,78],4);
+
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mymap);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
+
